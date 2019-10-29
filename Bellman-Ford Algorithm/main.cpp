3de057fa195b3690ec2d62316d18170317c69ea1
George <georgii.kashin@gmail.com>
2019-10-29 06:21:20 +0300

    Initial Commit

---
+++ Bellman-Ford Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Bellman-Ford Algorithm
+//
+//  Created by Георгий Кашин on 29.10.2019.
+//  Copyright © 2019 Georgii Kashin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
